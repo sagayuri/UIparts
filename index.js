0a858eeea19493bf31ff21b561e3807fa182062f
yuri <info@sagayuri.com>
2023-03-30 13:14:08 +0900

    ulのパディングを調整

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function() {
 		prevText:'前へ',//1つ前のページへ移動するボタンのテキスト
 		nextText:'次へ',//1つ次のページへ移動するボタンのテキスト
 		activeClass: 'navi-active',//現在のページ番号に任意のclassを付与できます
-        pageNumbers: true,
-        firstLast: false,
+        pageNumbers: true,//何件中何ページ目かの表示
+        firstLast: false,//最初へと最後への非表示
 	})
 });

3335cdb0de75dbb2885f490243070f9b02e8126b
yuri <info@sagayuri.com>
2023-03-30 13:06:33 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.news-container').paginathing({//親要素のclassを記述
+		perPage: 3,//1ページあたりの表示件数
+		prevText:'前へ',//1つ前のページへ移動するボタンのテキスト
+		nextText:'次へ',//1つ次のページへ移動するボタンのテキスト
+		activeClass: 'navi-active',//現在のページ番号に任意のclassを付与できます
+        pageNumbers: true,
+        firstLast: false,
+	})
+});

9c5db5a3c2df262c8a74caefe8bb7b4b04b03c1c
yuri <info@sagayuri.com>
2023-03-30 14:57:19 +0900

    custom-css

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ $(function() {
 		activeClass: 'navi-active',//現在のページ番号に任意のclassを付与できます
         pageNumbers: true,//何件中何ページ目かの表示
         firstLast: false,//最初へと最後への非表示
+		liClass: 'page-item',
 	})
 });
